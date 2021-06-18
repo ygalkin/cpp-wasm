4b48a7aa0acbbb94ebe58f9f7c06e0f44b05a365
ygalkin <y.galkin@gmail.com>
2021-06-18 16:25:31 -0700

    update

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,3 @@
 del out-app.*
-../../emcc cpp-wasm-example.cpp -O3 -s WASM=1 --no-entry -o ./out-app.js -s EXPORTED_RUNTIME_METHODS="['allocateUTF8OnStack', 'UTF8ToString', 'stackSave', 'stackRestore', 'stackAlloc']" 
+emcc cpp-wasm-example.cpp -O3 -s WASM=1 --no-entry -o ./out-app.js -s EXPORTED_RUNTIME_METHODS="['allocateUTF8OnStack', 'UTF8ToString', 'stackSave', 'stackRestore', 'stackAlloc']" 
 
